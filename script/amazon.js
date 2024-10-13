44e5dde3bd963fbc0dbd62160829ec11ccff8e01
Bhattii <imran5244513@gmail.com>
2024-10-13 01:26:56 -0700

    Save Cart to locak Storage

--- script/amazon.js
+++ script/amazon.js
@@ -60,11 +60,11 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 function updateCartQuantity() {
 	let cartQuantity = 0;
+
 	cart.forEach((cartItem) => {
 		cartQuantity += cartItem.quantity;
 	});
 	document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
-	console.log(cartQuantity);
 }
 
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
